63f5f5b7914883f54f5b3dd6a2611f79ca701155
junaidrt <junaid@rebeltechnology.io>
2022-04-08 17:02:16 +0500

    Added some comapny logos.

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+
+  <v-app-bar
+      app
+      color="primary"
+      dark
+  >
+    <div class="d-flex align-center">
+      <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://res-4.cloudinary.com/vervoe/image/upload/ar_810:145,dpr_1,w_200/hvn8q6n7fttaayikn5mx"
+          width="200"
+      />
+    </div>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+
+</style>
